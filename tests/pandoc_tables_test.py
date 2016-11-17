ec4c0eec163e4ea3008a63bb047a5968cdb03d54
Kolen Cheung <christian.kolen@gmail.com>
2016-11-17 02:17:17 -0800

    setup travis

---
+++ tests/pandoc_tables_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+
+"""
+from pandoc_tables import pandoc_tables
+
+
+def test_to_bool():
+    assert pandoc_tables.to_bool("true") == True
+    assert pandoc_tables.to_bool("false") == False
+    assert pandoc_tables.to_bool("yes") == True
+    assert pandoc_tables.to_bool("no") == False
+    assert pandoc_tables.to_bool("NO") == False
+    return
