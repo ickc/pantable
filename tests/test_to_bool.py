08ae092a684a65b15017d9f592b4f65876041410
Kolen Cheung <christian.kolen@gmail.com>
2016-11-17 10:28:12 -0800

    pep8 compliance

--- tests/test_to_bool.py
+++ tests/test_to_bool.py
@@ -6,9 +6,9 @@ from .context import pandoc_tables
 
 
 def test_to_bool():
-    assert pandoc_tables.to_bool("true") == True
-    assert pandoc_tables.to_bool("false") == False
-    assert pandoc_tables.to_bool("yes") == True
-    assert pandoc_tables.to_bool("no") == False
-    assert pandoc_tables.to_bool("NO") == False
+    assert pandoc_tables.to_bool("true")
+    assert pandoc_tables.to_bool("false") is False
+    assert pandoc_tables.to_bool("yes")
+    assert pandoc_tables.to_bool("no") is False
+    assert pandoc_tables.to_bool("NO") is False
     return
