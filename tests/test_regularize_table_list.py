f920b3b2045820232ad8a631fc22871fb753cccf
Kolen Cheung <christian.kolen@gmail.com>
2016-11-17 12:39:52 -0800

    pep8

--- tests/test_regularize_table_list.py
+++ tests/test_regularize_table_list.py
@@ -6,6 +6,7 @@ from .context import pandoc_tables
 
 
 def test_regularize_table_list():
-    raw_table_list = [['1'],['2','3','4','5','6','7']]
-    assert pandoc_tables.regularize_table_list(raw_table_list) == [['1', '', '', '', '', ''], ['2', '3', '4', '5', '6', '7']]
+    raw_table_list = [['1'], ['2', '3', '4', '5', '6', '7']]
+    assert pandoc_tables.regularize_table_list(raw_table_list) == [
+        ['1', '', '', '', '', ''], ['2', '3', '4', '5', '6', '7']]
     return
