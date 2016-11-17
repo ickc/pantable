e655aa3ff9554c0e1497672cbaf91e4fe7247b63
Kolen Cheung <christian.kolen@gmail.com>
2016-11-17 07:21:27 -0800

    tests/context.py: set path to `..`

--- tests/context.py
+++ tests/context.py
@@ -1,5 +1,5 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('pandoc_tables'))
+sys.path.insert(0, os.path.abspath('..'))
 
-import pandoc_tables
+from pandoc_tables import pandoc_tables
