db5ff22a4b6652346b55ccfda723488d6ddcab56
Kolen Cheung <christian.kolen@gmail.com>
2016-11-19 19:13:55 -0800

    pantable.py: functions on raw_table_list in-place, rather than reassign

--- tests/test_regularize_table_list.py
+++ tests/test_regularize_table_list.py
@@ -7,6 +7,9 @@ from .context import regularize_table_list
 
 def test_regularize_table_list():
     raw_table_list = [['1'], ['2', '3', '4', '5', '6', '7']]
-    assert regularize_table_list(raw_table_list) == [
-        ['1', '', '', '', '', ''], ['2', '3', '4', '5', '6', '7']]
+    regularize_table_list(raw_table_list)
+    assert raw_table_list == [
+        ['1', '', '', '', '', ''],
+        ['2', '3', '4', '5', '6', '7']
+    ]
     return
