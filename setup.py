9ac5b3cc68b2d4962ece776e7d14028d9bafc213
Kolen Cheung <christian.kolen@gmail.com>
2016-11-20 04:43:29 -0800

    setup.py: fix bug

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'pantable=pantable:main',
+            'pantable = pantable.pantable:main',
         ],
     },
 )
