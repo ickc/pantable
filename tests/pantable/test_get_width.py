6971e9cfb8a8f2e50a77a4915a9a27bf671e4bec
Kolen Cheung <christian.kolen@gmail.com>
2016-12-04 18:58:52 -0800

    pantable.py: reorganize get_width and parse_width

--- tests/pantable/test_get_width.py
+++ tests/pantable/test_get_width.py
@@ -7,17 +7,23 @@ def test_get_width():
     # check width
     # init
     options = {}
-    assert get_width(options) is None
+    assert get_width(options, 2) is None
     # negative width
     options['width'] = [0.1, -0.2]
-    assert get_width(options) is None
+    assert get_width(options, 2) is None
     # invalid width
     options['width'] = "happy"
-    assert get_width(options) is None
+    assert get_width(options, 1) is None
     # invalid width 2
     options['width'] = ["happy", "birthday"]
-    assert get_width(options) is None
+    assert get_width(options, 2) is None
     # fractional
     options['width'] = ["1/2", "1/10"]
-    assert get_width(options) == [0.5, 0.1]
+    assert get_width(options, 2) == [0.5, 0.1]
+    # width too short
+    options['width'] = [0.1, 0.2, 0.3]
+    assert get_width(options, 4) == None
+    # width too long
+    options['width'] = [0.1, 0.2, 0.3, 0.4, 0.5]
+    assert get_width(options, 4) == None
     return
