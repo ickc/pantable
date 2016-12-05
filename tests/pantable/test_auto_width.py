f26cb0ad363909c6ae65cd80b4f7865e581a034f
Kolen Cheung <christian.kolen@gmail.com>
2016-12-04 19:03:41 -0800

    pantable.py: rename parse_width to auto_width

--- tests/pantable/test_auto_width.py
+++ tests/pantable/test_auto_width.py
@@ -1,13 +1,13 @@
 """
 """
-from .context import parse_width
+from .context import auto_width
 
 
-def test_parse_width():
+def test_auto_width():
     raw_table_list = [
         ['asdfdfdfguhfdhghfdgkla', '334\n2', '**la**', '4'],
         ['5', '6', '7', '8']
     ]
-    assert parse_width(1.2, 4, raw_table_list) == [25 / 44 * 1.2,
+    assert auto_width(1.2, 4, raw_table_list) == [25 / 44 * 1.2,
                                                          6 / 44 * 1.2, 9 / 44 * 1.2, 4 / 44 * 1.2]
     return
