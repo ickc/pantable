67dd77b9eb4e6304abdad6cfc38bd2739e69c0fd
Kolen Cheung <christian.kolen@gmail.com>
2016-12-05 00:13:27 -0800

    pantable2csv.py: inline all functions

--- pantable/pantable2csv.py
+++ pantable/pantable2csv.py
@@ -57,101 +57,56 @@ import csv
 import yaml
 
 
-def ast2markdown(*ast):
-    """
-    convert a panflute ast into markdown
-    """
-    return panflute.convert_text(ast, input_format='panflute', output_format='markdown')
-
-
-def get_table_options(elem):
-    """
-    parse the content of Table in ast and returns a dictionary of options
-    """
-    options = {}
-    options['caption'] = elem.caption
-    options['alignment'] = elem.alignment
-    options['width'] = elem.width
-    options['header'] = elem.header
-    options['markdown'] = True
-    return options
-
-
-def parse_table_options(options):
-    """
-    parse the options
-    """
-    # caption: panflute ast to markdown
-    if options['caption']:
-        options['caption'] = ast2markdown(panflute.Para(*options['caption']))
-    else:
-        del options['caption']
-    # parse alignment
-    parsed_alignment = []
-    for alignment in options['alignment']:
-        if alignment == "AlignLeft":
-            parsed_alignment.append("L")
-        elif alignment == "AlignCenter":
-            parsed_alignment.append("C")
-        elif alignment == "AlignRight":
-            parsed_alignment.append("R")
-        elif alignment == "AlignDefault":
-            parsed_alignment.append("D")
-    options['alignment'] = "".join(parsed_alignment)
-    # table-width from width
-    options['table-width'] = sum(options['width'])
-    # header: False if empty header row, else True
-    options['header'] = bool(panflute.stringify(options['header']))
-    return
-
-
-def get_table_body(options, elem):
-    """
-    from elem, get full table body including header row if any
-    """
-    table_body = elem.content
-    if options['header']:
-        table_body.insert(0, elem.header)
-    return table_body
-
-
-def Table2list(Table):
-    """
-    convert a pandoc table into a 2D list
-    """
-    return [[ast2markdown(*cell.content) for cell in row.content] for row in Table]
-
-
-def list2csv(table_list):
-    with io.StringIO() as file:
-        writer = csv.writer(file)
-        writer.writerows(table_list)
-        csv_table = file.getvalue()
-    return csv_table
-
-
-def options2yaml(options):
-    return yaml.dump(options)
-
-
 def table2csv(elem, doc):
     """
     find Table element and return a csv table in code-block with class "table"
     """
     if isinstance(elem, panflute.Table):
-        # obtain options from Table
-        options = get_table_options(elem)
-        parse_table_options(options)
-        # table in AST
-        table_body = get_table_body(options, elem)
-        # table in list
-        table_list = Table2list(table_body)
-        # table in CSV
-        csv_table = list2csv(table_list)
+        # get options as a dictionary
+        options = {}
+        # options: caption: panflute ast to markdown
+        if elem.caption:
+            options['caption'] = panflute.convert_text(
+                panflute.Para(*elem.caption),
+                input_format='panflute',
+                output_format='markdown'
+            )
+        # options: alignment
+        parsed_alignment = [("L" if i == "AlignLeft"
+                             else "C" if i == "AlignCenter"
+                             else "R" if i == "AlignRight"
+                             else "D") for i in elem.alignment]
+        options['alignment'] = "".join(parsed_alignment)
+        # options: width
+        options['width'] = elem.width
+        # options: table-width from width
+        options['table-width'] = sum(options['width'])
+        # options: header: False if empty header row, else True
+        options['header'] = bool(panflute.stringify(elem.header))
+        # options: markdown
+        options['markdown'] = True
+
         # option in YAML
-        yaml_metadata = options2yaml(options)
-        code_block = "---\n" + yaml_metadata + "---\n" + csv_table
-        return panflute.CodeBlock(code_block, classes=["table"])
+        yaml_metadata = yaml.dump(options)
+
+        # table in panflute AST
+        table_body = elem.content
+        if options['header']:
+            table_body.insert(0, elem.header)
+        # table in list
+        table_list = [
+            [panflute.convert_text(
+                cell.content,
+                input_format='panflute',
+                output_format='markdown'
+            ) for cell in row.content]
+            for row in table_body]
+        # table in CSV
+        with io.StringIO() as file:
+            writer = csv.writer(file)
+            writer.writerows(table_list)
+            csv_table = file.getvalue()
+        return panflute.CodeBlock("---\n" + yaml_metadata + "---\n" + csv_table, classes=["table"])
     return None
 
 
