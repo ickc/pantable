6ab87756d0d6ff6c35caf7f5f29d54cca518ede2
Kolen Cheung <christian.kolen@gmail.com>
2017-01-13 16:54:22 -0800

    make autopep8

--- tests/pantable/test_convert2table.py
+++ tests/pantable/test_convert2table.py
@@ -4,6 +4,7 @@
 from .context import convert2table
 from panflute import *
 
+
 def test_convert2table():
     # normal table
     data = r'''
@@ -11,11 +12,13 @@ def test_convert2table():
 3,4
 '''
     table_converted = convert2table({'width': [0, 0]}, data)
-    table_referenced = Table(TableRow(TableCell(Plain(Str('1'))), TableCell(Plain(Str('2')))), TableRow(TableCell(Plain(Str('3'))), TableCell(Plain(Str('4')))), alignment=['AlignDefault', 'AlignDefault'], width=[0.0, 0.0])
+    table_referenced = Table(TableRow(TableCell(Plain(Str('1'))), TableCell(Plain(Str('2')))), TableRow(TableCell(
+        Plain(Str('3'))), TableCell(Plain(Str('4')))), alignment=['AlignDefault', 'AlignDefault'], width=[0.0, 0.0])
     assert repr(table_converted) == repr(table_referenced)
     # empty header_row
     table_converted = convert2table({'header': False}, data)
-    table_referenced = Table(TableRow(TableCell(Plain(Str(''))), TableCell(Plain(Str('')))), TableRow(TableCell(Plain(Str('1'))), TableCell(Plain(Str('2')))), TableRow(TableCell(Plain(Str('3'))), TableCell(Plain(Str('4')))), alignment=['AlignDefault', 'AlignDefault'], width=[0.5, 0.5])
+    table_referenced = Table(TableRow(TableCell(Plain(Str(''))), TableCell(Plain(Str('')))), TableRow(TableCell(Plain(Str('1'))), TableCell(Plain(
+        Str('2')))), TableRow(TableCell(Plain(Str('3'))), TableCell(Plain(Str('4')))), alignment=['AlignDefault', 'AlignDefault'], width=[0.5, 0.5])
     assert repr(table_converted) == repr(table_referenced)
     # empty table
     data = ','
@@ -24,7 +27,8 @@ def test_convert2table():
     # 1 row table
     data = '1,2'
     table_converted = convert2table({}, data)
-    table_referenced = Table(TableRow(TableCell(Plain(Str('1'))), TableCell(Plain(Str('2')))), alignment=['AlignDefault', 'AlignDefault'], width=[0.5, 0.5])
+    table_referenced = Table(TableRow(TableCell(Plain(Str('1'))), TableCell(
+        Plain(Str('2')))), alignment=['AlignDefault', 'AlignDefault'], width=[0.5, 0.5])
     assert repr(table_converted) == repr(table_referenced)
     # empty data
     table = convert2table({}, '')
