5a93db161b3f0e2a50d50137b7f367403e721728
Kolen Cheung <christian.kolen@gmail.com>
2019-05-20 23:19:16 -0700

    add `grid_tables`

---
+++ tests/pantable/test_csv_to_grid_tables.py
@@ -0,0 +1,60 @@
+"""
+"""
+from .context import csv_to_grid_tables
+import io
+import csv
+
+
+def test_csv_to_grid_tables():
+    data = [['**_Fruit_**', '~~Price~~', '_Number_', '`Advantages`'],
+            ['*Bananas~1~*',
+             '$1.34',
+             '12~units~',
+             'Benefits of eating bananas\n(**Note the appropriately\nrendered block markdown**):\n\n- _built-in wrapper_\n- ~~**bright color**~~\n\n'],
+            ['*Oranges~2~*',
+             '$2.10',
+             '5^10^~units~',
+             'Benefits of eating oranges:\n\n- **cures** scurvy\n- `tasty`']]
+
+    caption = 'Some *caption*.'
+    alignment = ['AlignLeft', 'AlignCenter', 'AlignRight', 'AlignDefault']
+
+    assert csv_to_grid_tables(data, caption, alignment, False) == '''+:-------------+:---------:+-------------:+-----------------------------+
+| **_Fruit_**  | ~~Price~~ | _Number_     | `Advantages`                |
++--------------+-----------+--------------+-----------------------------+
+| *Bananas~1~* | $1.34     | 12~units~    | Benefits of eating bananas  |
+|              |           |              | (**Note the appropriately   |
+|              |           |              | rendered block markdown**): |
+|              |           |              |                             |
+|              |           |              | - _built-in wrapper_        |
+|              |           |              | - ~~**bright color**~~      |
+|              |           |              |                             |
+|              |           |              |                             |
++--------------+-----------+--------------+-----------------------------+
+| *Oranges~2~* | $2.10     | 5^10^~units~ | Benefits of eating oranges: |
+|              |           |              |                             |
+|              |           |              | - **cures** scurvy          |
+|              |           |              | - `tasty`                   |
++--------------+-----------+--------------+-----------------------------+
+
+: Some *caption*.'''
+
+    assert csv_to_grid_tables(data, caption, alignment, True) == '''+--------------+-----------+--------------+-----------------------------+
+| **_Fruit_**  | ~~Price~~ | _Number_     | `Advantages`                |
++:=============+:=========:+=============:+=============================+
+| *Bananas~1~* | $1.34     | 12~units~    | Benefits of eating bananas  |
+|              |           |              | (**Note the appropriately   |
+|              |           |              | rendered block markdown**): |
+|              |           |              |                             |
+|              |           |              | - _built-in wrapper_        |
+|              |           |              | - ~~**bright color**~~      |
+|              |           |              |                             |
+|              |           |              |                             |
++--------------+-----------+--------------+-----------------------------+
+| *Oranges~2~* | $2.10     | 5^10^~units~ | Benefits of eating oranges: |
+|              |           |              |                             |
+|              |           |              | - **cures** scurvy          |
+|              |           |              | - `tasty`                   |
++--------------+-----------+--------------+-----------------------------+
+
+: Some *caption*.'''
