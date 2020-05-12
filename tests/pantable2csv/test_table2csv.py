fb7caf6f53bcdccc54a481b35a8b62601dc1d737
Kolen Cheung <christian.kolen@gmail.com>
2020-05-11 18:01:04 -0700

    test_table2csv.py: fix code_block_referenced

--- tests/pantable2csv/test_table2csv.py
+++ tests/pantable2csv/test_table2csv.py
@@ -22,7 +22,11 @@ caption: '*abcd*'
 header: true
 markdown: true
 table-width: 0
-width: [0, 0, 0, 0]
+width:
+- 0
+- 0
+- 0
+- 0
 ---
 1,2,3,4\r\n1,2,3,4\r\n''', classes=['table'])
     assert repr(code_block_converted) == repr(code_block_referenced)
