f27d64af6b94fe2fe2c013a75b5c142f8ea7c129
Kolen Cheung <christian.kolen@gmail.com>
2020-05-12 00:25:22 -0700

    setup.py: drop 3.4 add 3.8 and pypy support

--- setup.py
+++ setup.py
@@ -76,11 +76,12 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         # https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
 
     # What does your project relate to?
