781fa36a665d00713070957c2dcba34c42d8d76f
Kolen Cheung <christian.kolen@gmail.com>
2020-11-17 15:19:21 -0800

    util.py: add table_for_pprint

--- pantable/util.py
+++ pantable/util.py
@@ -12,3 +12,24 @@ def ast_to_markdown(ast):
         input_format='panflute',
         output_format='markdown'
     )
+
+
+def table_for_pprint(table: panflute.Table):
+    '''represent panflute Table in a dict structure for pprint
+
+    >>> pprint(table_for_pprint(table), sort_dicts=False, compact=False, width=-1)
+
+    Note:
+
+    Each HeadRow, BodyRow, FootRow, Cell also has ica not shown here.
+
+    TableBody has RowHeadColumns int
+    '''
+    return {
+        'Table': (table.identifier, table.classes, table.attributes),
+        'Caption': (tuple(short_caption) if (short_caption := table.caption.short_caption) else None, tuple(table.caption.content)),
+        'specs': table.colspec,
+        'TableHead': [list(row.content) for row in table.head.content],
+        'TableBody': [list(row.content) for row in table.content[0].content],
+        'TableFoot': [list(row.content) for row in table.foot.content],
+    }
