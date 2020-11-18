672093dccfbc4c1a4bb2115b106d2197c09dba95
Kolen Cheung <christian.kolen@gmail.com>
2020-11-18 15:58:18 -0800

    ast.py: batch rename

--- pantable/ast.py
+++ pantable/ast.py
@@ -217,7 +217,7 @@ class PanTable(FakeRepr, AlignText):
         short_caption: Optional[ListContainer], caption: ListContainer,
         spec: Spec,
         ms: np.ndarray[np.int64], n: int, ns_head: np.ndarray[np.int64],
-        icas_row_block: np.ndarray[Ica],
+        icas_rowblock: np.ndarray[Ica],
         icas_row: np.ndarray[Ica],
         icas: np.ndarray[Ica],
         aligns: np.ndarray[np.int8],
@@ -230,7 +230,7 @@ class PanTable(FakeRepr, AlignText):
         self._ms = ms
         self.n = n
         self.ns_head = ns_head
-        self.icas_row_block = icas_row_block
+        self.icas_rowblock = icas_rowblock
         self.icas_row = icas_row
         self.icas = icas
         self.aligns = aligns
@@ -246,7 +246,7 @@ class PanTable(FakeRepr, AlignText):
             'shape': self.shape,
             'ms': self.ms,
             'ns_head': self.ns_head,
-            'icas_row_block': self.icas_row_block,
+            'icas_rowblock': self.icas_rowblock,
             'icas_row': self.icas_row,
             'icas': self.icas,
             'aligns': self.aligns_text,
@@ -258,20 +258,24 @@ class PanTable(FakeRepr, AlignText):
         return (self._ms.sum(), self.n)
 
     @property
-    def n_bodies(self) -> int:
+    def m_bodies(self) -> int:
         return self.ns_head.size
 
     @property
+    def m_rowblocks(self) -> int:
+        return self._ms.size
+
+    @property
     def ica_head(self) -> Ica:
-        return self.icas_row_block[0]
+        return self.icas_rowblock[0]
 
     @property
     def icas_body(self) -> np.ndarray[Ica]:
-        return self.icas_row_block[1:-1]
+        return self.icas_rowblock[1:-1]
 
     @property
     def ica_foot(self) -> Ica:
-        return self.icas_row_block[-1]
+        return self.icas_rowblock[-1]
 
     @property
     def ms(self) -> np.ndarray[np.int64]:
@@ -279,32 +283,32 @@ class PanTable(FakeRepr, AlignText):
 
     @ms.setter
     def ms(self, ms):
-        del self.idxs_ms
-        del self.idxs_split
+        del self.rowblock_idxs_row
+        del self.rowblock_splitting_idxs
         del self.is_heads
         del self.is_foots
         del self.is_body_heads
         del self.is_body_bodies
-        del self.idxs_body
+        del self.body_idxs_row
         self._ms = ms
 
     @cached_property
-    def idxs_ms(self) -> np.ndarray[np.int64]:
-        '''reverse lookup the index of rows in blocks of ms
+    def rowblock_idxs_row(self) -> np.ndarray[np.int64]:
+        '''reverse lookup the index of rowblocks per row
         '''
-        return np.digitize(np.arange(self.shape[0]), np.cumsum(self.ms))
+        return np.digitize(np.arange(self.shape[0]), np.cumsum(self._ms))
 
     @cached_property
     def is_heads(self) -> np.ndarray[np.bool_]:
-        return self.idxs_ms == 0
+        return self.rowblock_idxs_row == 0
 
     @cached_property
     def is_foots(self) -> np.ndarray[np.bool_]:
-        return self.idxs_ms == (self.ms.size - 1)
+        return self.rowblock_idxs_row == (self._ms.size - 1)
 
     @cached_property
     def is_body_heads(self) -> np.ndarray[np.bool_]:
-        maybe_body_heads = self.idxs_ms % 2 == 1
+        maybe_body_heads = self.rowblock_idxs_row % 2 == 1
         return (~self.is_foots) & maybe_body_heads
 
     @cached_property
@@ -312,44 +316,44 @@ class PanTable(FakeRepr, AlignText):
         return ~(self.is_heads | self.is_foots | self.is_body_heads)
 
     @cached_property
-    def idxs_body(self) -> np.ndarray[np.int64]:
+    def body_idxs_row(self) -> np.ndarray[np.int64]:
         '''calculate the i-th body that each row belongs to
 
         negative values means the row is not in a body
         '''
-        idxs_body = (self.idxs_ms - 1) // 2
-        idxs_body[self.is_foots] = -1
-        return idxs_body
+        body_idxs_row = (self.rowblock_idxs_row - 1) // 2
+        body_idxs_row[self.is_foots] = -1
+        return body_idxs_row
 
     @cached_property
-    def idxs_split(self) -> np.ndarray[np.int64]:
-        '''applying np.split(array_of_all_rows, idxs_split) would break it back into list of head, bodies, foot
+    def rowblock_splitting_idxs(self) -> np.ndarray[np.int64]:
+        '''applying np.split(array_of_rows, rowblock_splitting_idxs) would break it back into list of head, bodies, foot
         '''
-        return np.cumsum(self.ms)[:-1]
+        return np.cumsum(self._ms)[:-1]
 
-    def iter_row_blocks(self, array: np.ndarray) -> List[np.ndarray]:
+    def iter_rowblocks(self, array: np.ndarray) -> List[np.ndarray]:
         '''break array into list of head, bodies, foot
 
         assume array is iterables of rows
         '''
-        return np.split(array, self.idxs_split)
+        return np.split(array, self.rowblock_splitting_idxs)
 
     def iterrows(self):
-        idxs_ms = self.idxs_ms
+        rowblock_idxs_row = self.rowblock_idxs_row
         is_heads = self.is_heads
         is_foots = self.is_foots
         is_body_heads = self.is_body_heads
         is_body_bodies = self.is_body_bodies
-        idxs_body = self.idxs_body
+        body_idxs_row = self.body_idxs_row
 
         res = []
         for i in range(self.shape[0]):
-            idx_block = idxs_ms[i]
+            idx_block = rowblock_idxs_row[i]
             is_head = is_heads[i]
             is_body_head = is_body_heads[i]
             is_body_body = is_body_bodies[i]
             is_foot = is_foots[i]
-            idx_body = idxs_body[i]
+            idx_body = body_idxs_row[i]
             idx_body = None if idx_body < 0 else idx_body
             res.append({
                 'is_head': is_head,
@@ -358,7 +362,7 @@ class PanTable(FakeRepr, AlignText):
                 'is_foot': is_foot,
                 'idx_body': idx_body,
                 'n_head': None if idx_body is None else self.ns_head[idx_body],
-                'ica_row_block': self.icas_row_block[idx_block],
+                'ica_row_block': self.icas_rowblock[idx_block],
                 'ica_row': self.icas_row[i],
                 'icas': self.icas[i],
                 'aligns': self.aligns[i],
@@ -367,7 +371,7 @@ class PanTable(FakeRepr, AlignText):
         return res
 
     @staticmethod
-    def iter_tablerow(
+    def iter_tablerows(
         icas_row: np.ndarray[Ica],
         pf_cells: np.ndarray[TableCell],
     ) -> List[TableRow]:
@@ -414,14 +418,14 @@ class PanTable(FakeRepr, AlignText):
         foot = table.foot
 
         bodies = table.content
-        n_bodies = len(bodies)
-        ns_head = np.empty(n_bodies, dtype=np.int64)
-        icas_row_block = np.empty(n_bodies + 2, dtype='O')
-        icas_row_block[0] = Ica(head.identifier, head.classes, head.attributes)
+        m_bodies = len(bodies)
+        ns_head = np.empty(m_bodies, dtype=np.int64)
+        icas_rowblock = np.empty(m_bodies + 2, dtype='O')
+        icas_rowblock[0] = Ica(head.identifier, head.classes, head.attributes)
         for i, body in enumerate(bodies):
             ns_head[i] = body.row_head_columns
-            icas_row_block[i + 1] = Ica(body.identifier, body.classes, body.attributes)
-        icas_row_block[i + 2] = Ica(foot.identifier, foot.classes, foot.attributes)
+            icas_rowblock[i + 1] = Ica(body.identifier, body.classes, body.attributes)
+        icas_rowblock[i + 2] = Ica(foot.identifier, foot.classes, foot.attributes)
 
         # there are 1 head,
         # then n bodies, for each body one head and one content,
@@ -468,7 +472,7 @@ class PanTable(FakeRepr, AlignText):
             short_caption, caption,
             spec,
             ms, n, ns_head,
-            icas_row_block,
+            icas_rowblock,
             icas_row,
             icas,
             aligns,
@@ -483,34 +487,34 @@ class PanTable(FakeRepr, AlignText):
 
         colspec = self.spec.to_panflute_ast()
 
-        icas_row_by_blocks = self.iter_row_blocks(self.icas_row)
-        pf_cells_by_blocks = self.iter_row_blocks(pancell_to_panflute_tablecell(
+        icas_row_by_blocks = self.iter_rowblocks(self.icas_row)
+        pf_cells_by_blocks = self.iter_rowblocks(pancell_to_panflute_tablecell(
             self.icas,
             self.aligns_text,
             self.cells_cannonical,
         ))
 
         # head
-        ica_block = self.icas_row_block[0]
-        icas_row_block = icas_row_by_blocks[0]
+        ica_block = self.icas_rowblock[0]
+        icas_rowblock = icas_row_by_blocks[0]
         pf_cells_block = pf_cells_by_blocks[0]
-        content = self.iter_tablerow(icas_row_block, pf_cells_block)
+        content = self.iter_tablerows(icas_rowblock, pf_cells_block)
         head = TableHead(*content, identifier=ica_block.identifier, classes=ica_block.classes, attributes=ica_block.attributes)
         # bodies
         bodies = []
-        for i in range(self.n_bodies):
+        for i in range(self.m_bodies):
             row_head_columns = int(self.ns_head[i])
             # offset 1 as 1st is head
-            ica_block = self.icas_row_block[1 + i]
+            ica_block = self.icas_rowblock[1 + i]
             temp = []
             for j in range(2):
                 # offset 1 as 1st is head
                 # 2 * i as 2 elements per body
                 # 1st is body-head, 2nd is body-body
                 idx_body = 1 + 2 * i + j
-                icas_row_block = icas_row_by_blocks[idx_body]
+                icas_rowblock = icas_row_by_blocks[idx_body]
                 pf_cells_block = pf_cells_by_blocks[idx_body]
-                temp.append(self.iter_tablerow(icas_row_block, pf_cells_block))
+                temp.append(self.iter_tablerows(icas_rowblock, pf_cells_block))
             bodies.append(TableBody(
                 *temp[1],
                 head=temp[0],
@@ -520,10 +524,10 @@ class PanTable(FakeRepr, AlignText):
                 attributes=ica_block.attributes,
             ))
         # foot
-        ica_block = self.icas_row_block[-1]
-        icas_row_block = icas_row_by_blocks[-1]
+        ica_block = self.icas_rowblock[-1]
+        icas_rowblock = icas_row_by_blocks[-1]
         pf_cells_block = pf_cells_by_blocks[-1]
-        content = self.iter_tablerow(icas_row_block, pf_cells_block)
+        content = self.iter_tablerows(icas_rowblock, pf_cells_block)
         foot = TableFoot(*content, identifier=ica_block.identifier, classes=ica_block.classes, attributes=ica_block.attributes)
 
         return Table(
