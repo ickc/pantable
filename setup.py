068b0dc2883f918c99a688120b4fcd59e538ca43
Kolen Cheung <christian.kolen@gmail.com>
2020-11-17 17:34:06 -0800

    setup.py: require panflute>=2

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['panflute>=1.8.2,<2', 'pyyaml'],
+    install_requires=['panflute>=2', 'pyyaml'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
