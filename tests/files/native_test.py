a5c34bafef13e5ac4bda6f2ced92889e46af2cd1
Kolen Cheung <christian.kolen@gmail.com>
2020-11-18 19:26:07 -0800

    native_test.py: refactor into one test per input file

--- tests/files/native_test.py
+++ tests/files/native_test.py
@@ -1,34 +1,42 @@
 from pathlib import Path
 import sys
+import inspect
 
 from panflute import convert_text
 
 from pantable.ast import PanTable
 
 
-def test_from_panflute_ast():
+def routine():
     '''test parsing native table into Pantable
     '''
-    paths = (Path(__file__).parent / 'native').glob('*.native')
-    for path in paths:
-        try:
-            with open(path, 'r') as f:
-                native = f.read()
-            temp = convert_text(native, input_format='native')
-            # input files should only have 1 single outter block
-            assert len(temp) == 1
-            table = temp[0]
-            pan_table = PanTable.from_panflute_ast(table)
-            table_idem = pan_table.to_panflute_ast()
-            # check for idempotence
-            native_orig = convert_text(table, input_format='panflute', output_format='native')
-            native_idem = convert_text(table_idem, input_format='panflute', output_format='native')
-            if native_orig == native_idem:
-                print(f'{path} is identical after round trip.', file=sys.stderr)
-            else:
-                raise AssertionError(f'{path} is not identical after round trip.')
-        except AssertionError as e:
-            raise e
-        except Exception as e:
-            print(f'Cannot parse native file {path} into PanTable.', file=sys.stderr)
-            raise e
+    # c.f. https://stackoverflow.com/a/5067654
+    name = inspect.stack()[1][3].split('_')[1]
+
+    path = Path(__file__).parent / 'native' / f'{name}.native'
+    print(f'Testing case {name} in {path}...', file=sys.stderr)
+    with open(path, 'r') as f:
+        native = f.read()
+    doc = convert_text(native, input_format='native')
+    # input files should only have 1 single outter block
+    assert len(doc) == 1
+    table = doc[0]
+    pan_table = PanTable.from_panflute_ast(table)
+    table_idem = pan_table.to_panflute_ast()
+    # check for idempotence
+    native_orig = convert_text(table, input_format='panflute', output_format='native')
+    native_idem = convert_text(table_idem, input_format='panflute', output_format='native')
+    assert native_orig == native_idem
+
+# test_NAME will test against the file NAME.native
+
+def test_nordics():
+    routine()
+
+
+def test_planets():
+    routine()
+
+
+def test_students():
+    routine()
