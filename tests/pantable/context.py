099c7ab30d23b899e978789fcc3ce5b619c75127
Kolen Cheung <christian.kolen@gmail.com>
2020-12-06 17:33:41 -0800

    remove obsolete modules and their tests

--- tests/pantable/context.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..' + os.sep + '..'))
-
-from pantable.read_csv import read_csv, regularize_table_list, parse_alignment
-from pantable.csv_to_table_markdown import modified_align_border, csv_to_grid_tables, csv_to_pipe_tables, csv_to_table_markdown
-from pantable.csv_to_table_ast import get_width, get_table_width, auto_width, parse_table_list, get_width_wrap, get_caption, csv_to_table_ast
-from pantable.codeblock_to_table import codeblock_to_table
