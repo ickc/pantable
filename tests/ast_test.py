614c80eed3522d3a1ec9669e587b57af14f937dd
Kolen Cheung <christian.kolen@gmail.com>
2020-12-06 21:31:26 -0800

    ast_test.py: use pytest.mark.parametrize instead

--- tests/ast_test.py
+++ tests/ast_test.py
@@ -1,40 +1,49 @@
 import numpy as np
 
+from pytest import mark
+
 from pantable.ast import PanTableOption, Align
 
-case_default = PanTableOption()
+
+@mark.parametrize('kwargs1,kwargs2', (
+    (
+        {'table_width': 3.},
+        {'table_width': 3},
+    ),
+    (
+        {},
+        {'table_width': 'string'},
+    ),
+    (
+        {'caption': '2'},
+        {'caption': 2},
+    ),
+    (
+        {},
+        {'width': [1, 2, None]},
+    ),
+    (
+        {'width': [1, 2, '2/3']},
+        {'width': [1, 2, 2 / 3]},
+    ),
+    (
+        {},
+        {'csv_kwargs': []},
+    ),
+    (
+        {'table_width': '2/3'},
+        {'table_width': 2 / 3},
+    ),
+))
+def test_pantableoption_type(kwargs1, kwargs2):
+    assert PanTableOption(**kwargs1) == PanTableOption(**kwargs2)
+
+
 case_test = PanTableOption.from_kwargs(**{
     'caption': 'Some interesting...',
     'unknown-key': 'path towards error',
     'table-width': 0.5,
 })
-
-def test_pantableoption_type_1():
-    assert PanTableOption(table_width=3.) == PanTableOption(table_width=3)
-
-
-def test_pantableoption_type_2():
-    assert case_default == PanTableOption(table_width='string')
-
-
-def test_pantableoption_type_3():
-    assert PanTableOption(caption='2') == PanTableOption(caption=2)
-
-
-def test_pantableoption_type_4():
-    assert case_default == PanTableOption(width=[1, 2, None])
-
-
-def test_pantableoption_type_5():
-    assert PanTableOption(width=[1, 2, '2/3']) == PanTableOption(width=[1, 2, 2 / 3])
-
-
-def test_pantableoption_type_6():
-    assert case_default == PanTableOption(csv_kwargs=[])
-
-
-def test_pantableoption_type_7():
-    assert PanTableOption(table_width='2/3') == PanTableOption(table_width=2 / 3)
 
 
 def test_pantableoption_unknown_key():
