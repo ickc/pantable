83841edc21e4a74ee57db405c01bc010e66bc283
Kolen Cheung <christian.kolen@gmail.com>
2020-12-08 19:47:28 -0800

    __init__.py: add logger

--- pantable/__init__.py
+++ pantable/__init__.py
@@ -1,1 +1,20 @@
 from .version import __version__
+
+import logging
+import os
+
+try:
+    from coloredlogs import ColoredFormatter as Formatter
+except ImportError:
+    from logging import Formatter
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+logger.addHandler(handler)
+handler.setFormatter(Formatter('%(name)s %(levelname)s %(message)s'))
+try:
+    level = os.environ.get('PANTABLELOGLEVEL', logging.WARNING)
+    logger.setLevel(level=level)
+except ValueError:
+    logger.setLevel(level=logging.WARNING)
+    logger.error(f'Unknown PANTABLELOGLEVEL {level}, set to default WARNING.')
