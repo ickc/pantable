8cdc740603fea26eb3f2bda4d7e97ceb1f2217aa
Kolen Cheung <christian.kolen@gmail.com>
2020-12-08 20:58:18 -0800

    native_test.py: cleanup

--- tests/files/native_test.py
+++ tests/files/native_test.py
@@ -42,40 +42,36 @@ def test_table_to_codeblock(name):
     assert res[0].strip() == res[1].strip()
 
 
-def read_table_to_codeblock_not_fancy(path: Path, path_ref: Path) -> Tuple[str, str]:
+def read_table_to_codeblock_not_fancy(path: Path):
     '''test parsing native to markdown codeblock without fancy-table
 
     This is lossy so we only check it runs
     '''
-    logger.info(f'Comparing {path} and {path_ref}...')
+    logger.info(f'Loading {path}...')
     with open(path, 'r') as f:
         text = f.read()
-    with open(path_ref, 'r') as f:
-        md_reference = f.read()
 
     doc = convert_text(text, input_format='native')
     # input files should only have 1 single outter block
     assert len(doc) == 1
     table = doc[0]
-    doc_out = table_to_codeblock(table, doc, fancy_table=False)
+    table_to_codeblock(table, doc, fancy_table=False)
 
 
 @mark.parametrize('name', (path.stem for path in DIRS[0].glob(f'*.{EXTs[0]}')))
 def test_table_to_codeblock_not_fancy(name):
-    paths = [dir_ / f'{name}.{ext}' for dir_, ext in zip(DIRS, EXTs)]
-    read_table_to_codeblock_not_fancy(*paths)
+    path = DIRS[0] / f'{name}.{EXTs[0]}'
+    read_table_to_codeblock_not_fancy(path)
 
 
-def read_table_to_codeblock_str(path: Path, path_ref: Path) -> Tuple[str, str]:
+def read_table_to_codeblock_str(path: Path):
     '''test parsing native to markdown codeblock without markdown
 
     This is lossy so we only check it runs
     '''
-    logger.info(f'Comparing {path} and {path_ref}...')
+    logger.info(f'Loading {path}...')
     with open(path, 'r') as f:
         text = f.read()
-    with open(path_ref, 'r') as f:
-        md_reference = f.read()
 
     doc = convert_text(text, input_format='native')
     # input files should only have 1 single outter block
@@ -83,10 +79,10 @@ def read_table_to_codeblock_str(path: Path, path_ref: Path) -> Tuple[str, str]:
     table = doc[0]
     pan_table = PanTable.from_panflute_ast(table)
     pan_table_str = pan_table.to_pantablestr()
-    pan_codeblock = pan_table_str.to_pancodeblock()
+    pan_table_str.to_pancodeblock()
 
 
 @mark.parametrize('name', (path.stem for path in DIRS[0].glob(f'*.{EXTs[0]}')))
 def test_table_to_codeblock_str(name):
-    paths = [dir_ / f'{name}.{ext}' for dir_, ext in zip(DIRS, EXTs)]
-    read_table_to_codeblock_str(*paths)
+    path = DIRS[0] / f'{name}.{EXTs[0]}'
+    read_table_to_codeblock_str(path)
