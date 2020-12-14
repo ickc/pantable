acf191723be173aa0dcd68424af0cbd3be38c3d7
Kolen Cheung <christian.kolen@gmail.com>
2020-12-13 17:17:02 -0800

    require cached_property if python<3.8

--- setup.py
+++ setup.py
@@ -90,7 +90,12 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['panflute>=2', 'pyyaml', 'numpy'],
+    install_requires=[
+        'panflute>=2',
+        'pyyaml',
+        'numpy',
+        'backports.cached_property; python_version < "3.8"'
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
